c8fe4750ad64bae0cada37358096a38c85ff2e31
sarweshkumar47 <sarweshkumar47@gmail.com>
2021-07-03 16:14:26 +0530

    Add basic UI layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+ 
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LeafletMapComponent } from './main-content/leaflet-map/leaflet-map.component';
+import { SidebarViewComponent } from './main-content/sidebar-view/sidebar-view.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MainContentComponent,
+    LeafletMapComponent,
+    SidebarViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
