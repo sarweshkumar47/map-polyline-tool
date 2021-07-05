664d3b7c652cb62d6d4f368c5fdde805404a1c5f
sarweshkumar47 <sarweshkumar47@gmail.com>
2021-07-05 11:53:24 +0530

    Add leaflet map

--- src/app/main-content/leaflet-map/leaflet-map.component.ts
+++ src/app/main-content/leaflet-map/leaflet-map.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { STREETMAP_TILE_LAYER, STREETMAP_ATTRIBUTION } from 'src/app/shared/app-constants';
+import { SATALLITEMAP_TILE_LAYER, SATALLITEMAP_ATTRIBUTION } from 'src/app/shared/app-constants';
 
 @Component({
   selector: 'app-leaflet-map',
   templateUrl: './leaflet-map.component.html',
   styleUrls: ['./leaflet-map.component.css']
 })
-export class LeafletMapComponent implements OnInit {
+export class LeafletMapComponent implements OnInit, AfterViewInit {
+
+  private leafletMap: L.Map;
+
+  private mapCenter: L.LatLngExpression = [ 30.73, 76.78 ];
+  private defaultZoom = 5;
+  private minZoom = 1;
+  private maxZoom = 20;
+
+  private zoomControl = true;
+
+  baseLayers = {
+    'Street': L.tileLayer(STREETMAP_TILE_LAYER, { attribution: STREETMAP_ATTRIBUTION }),
+    'Satellite': L.tileLayer(SATALLITEMAP_TILE_LAYER, { attribution: SATALLITEMAP_ATTRIBUTION })
+  };
 
   constructor() { }
+  
 
   ngOnInit(): void {
+    
+  }
+
+  private initMap(): void {
+    this.leafletMap = L.map('leaflet-map', {
+      center: this.mapCenter,
+      minZoom: this.minZoom,
+      zoom: this.defaultZoom,
+      maxZoom: this.maxZoom,
+      zoomControl: this.zoomControl
+    });
+  }
+
+  setupMapLayers(map: L.Map) {
+    L.control.layers(this.baseLayers, null, {collapsed: true}).addTo(map);
+    this.baseLayers["Satellite"].addTo(map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.setupMapLayers(this.leafletMap);
   }
 
 }
---
+++ src/app/shared/app-constants.ts
@@ -0,0 +1,8 @@
+export const STREETMAP_TILE_LAYER = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+export const STREETMAP_ATTRIBUTION = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+export const SATALLITEMAP_TILE_LAYER = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+export const SATALLITEMAP_ATTRIBUTION = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+
+export const WIKIMEDIAMAP_TILE_LAYER = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png';
+export const WIKIMEDIA_ATTRIBUTION = '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>';
